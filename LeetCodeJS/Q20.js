4cad656dbf0b452c1e9cd6bc6be732490a023ebd
Irfankhan761 <irfandk1994@gmail.com>
2024-08-26 22:25:38 +0500

    LeetCodeJS

--- LeetCodeJS/Q20.js
+++ LeetCodeJS/Q20.js
@@ -1,6 +1,6 @@
 // JSON
 
-// Q19:Is Object Empty (Easy)
+// Q20:Is Object Empty (Easy)
 // Given an object or an array, return if it is empty.
 // An empty object contains no key-value pairs.
 // An empty array contains no elements.
