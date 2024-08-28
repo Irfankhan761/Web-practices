04834902339337787ddec931960f2aae78dcf327
Irfankhan761 <irfandk1994@gmail.com>
2024-08-29 00:01:30 +0500

    LeetCodeJS

--- LeetCodeJS/Q23.js
+++ LeetCodeJS/Q23.js
@@ -1,6 +1,6 @@
 // JSON
 
-// Q22:Group By (Medium)
+// Q23:Group By (Medium)
 // Write code that enhances all arrays such that you can call the array.groupBy(fn) method on any array and it will return a grouped version of the array.
 // A grouped array is an object where each key is the output of fn(arr[i]) and each value is an array containing all items in the original array with that key.
 // The provided callback fn will accept an item in the array and return a string key.
