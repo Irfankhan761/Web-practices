e1d384250231c22620c15659bad9a48003ccb5a8
Irfankhan761 <irfandk1994@gmail.com>
2024-08-29 00:02:50 +0500

    Update Q24.js

--- LeetCodeJS/Q24.js
+++ LeetCodeJS/Q24.js
@@ -1,6 +1,6 @@
 // JSON
 
-// Q22:2724. Sort By (Easy)
+// Q24:Sort By (Easy)
 // Given an array arr and a function fn, return a sorted array sortedArr. You can assume fn only returns numbers and those numbers
 // determine the sort order of sortedArr. sortedArray must be sorted in ascending order by fn output.
 // You may assume that fn will never duplicate numbers for a given array.
